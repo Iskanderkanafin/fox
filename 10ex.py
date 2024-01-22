4dd5f84a885ec7aed3c54178313d1ad0f08e1b86
Iskandir <i_kanafin@kbtu.kz>
2024-01-22 22:59:43 +0600

    11com

--- 10ex.py
+++ 10ex.py
@@ -1,4 +1,4 @@
-Insert the correct syntax to assign the same value to all three variables in one code line.
+#Insert the correct syntax to assign the same value to all three variables in one code line.
 
 
 x = y = z = "Orange"
