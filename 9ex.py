0d2fdbea5abde2baa22af666e2418196aa9930ae
Iskandir <i_kanafin@kbtu.kz>
2024-01-22 23:00:29 +0600

    11com

--- 9ex.py
+++ 9ex.py
@@ -1,4 +1,4 @@
-Insert the correct syntax to assign values to multiple variables in one line:
+#Insert the correct syntax to assign values to multiple variables in one line:
 
 
 x, y, z = "Orange", "Banana", "Cherry"
