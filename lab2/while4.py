c640d434970d34b8f24d714817ff2c46651636f6
Iskandir <i_kanafin@kbtu.kz>
2024-01-28 01:36:33 +0600

    4com

--- lab2/while4.py
+++ lab2/while4.py
@@ -1,6 +1,10 @@
-#In the loop, when i is 3, jump directly to the next iteration.
+#Print a message once the condition is false.
 
- i = 0 while i < 6:  i += 1  if i == 3:  
-continue
 
- print(i)
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+
+  print("i is no longer less than 6")
