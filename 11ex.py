2b54bf2bad5ab1baf468506a939488435bf12fbb
Iskandir <i_kanafin@kbtu.kz>
2024-02-06 21:07:05 +0600

    11com

--- 11ex.py
+++ 11ex.py
@@ -1,6 +1,6 @@
 #Insert the correct keyword to make the variable x belong to the global scope.
-
+#В Python ключевое слово global позволяет изменять переменную за пределами текущей области видимости. Оно используется для создания глобальной переменной и внесения в нее изменений в локальном контексте.
 
 def myfunc():
-global x
+  global x
   x = "fantastic"
